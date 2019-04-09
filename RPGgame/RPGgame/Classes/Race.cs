890a6921663a24c00ce77ef8b011b017f85ade07
iangifford <lightyagamie10@gmail.com>
2019-04-09 14:21:25 -0700

    Adds classes and building out race class

---
+++ RPGgame/RPGgame/Classes/Race.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGgame.Classes
+{
+   public abstract class Race
+   {
+        public abstract bool Human { get; set; }
+        public abstract bool Elf { get; set; }
+        public abstract bool Orc { get; set; }
+        public abstract bool DragonBorn { get; set; }
+        public abstract bool Pikachu { get; set; }
+        public virtual bool IsStrong { get; set; }
+
+    }
+}
