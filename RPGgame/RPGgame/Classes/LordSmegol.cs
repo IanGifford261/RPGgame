d902924ffca04ce40f8ec7947e99b9990d34b418
iangifford <lightyagamie10@gmail.com>
2019-04-09 14:35:01 -0700

    LordSmegol built out

--- RPGgame/RPGgame/Classes/LordSmegol.cs
+++ RPGgame/RPGgame/Classes/LordSmegol.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace RPGgame.Classes
 {
-    class LordSmegol
-    {
+    public class LordSmegol : Warrior
+    {       
+        public override bool Human { get; set; } = false;
+        public override bool Elf { get; set; } = false;
+        public override bool Orc { get; set; } = true;
+        public override bool DragonBorn { get; set; } = false;
+        public override bool Pikachu { get; set; } = false;
+        public override bool IsStrong { get; set; } = true;
+
+        public override int Strike()
+        {
+            return 92;
+        }
+
+        public override int Devestate()
+        {
+            return 280;
+        }
     }
 }
